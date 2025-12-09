443a449780558ba02aed7690627421e5e1979719
sothearo-kay <sothearo.kay@techostartup.center>
2025-12-09 15:20:11 +0700

    feat(ui): add nuxt ui

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 // @ts-check
 import antfu from "@antfu/eslint-config";
+import eslintPluginBetterTailwindcss from "eslint-plugin-better-tailwindcss";
 import withNuxt from "./.nuxt/eslint.config.mjs";
 
 export default withNuxt(
@@ -12,6 +13,22 @@ export default withNuxt(
         indent: 2,
         quotes: "double",
         semi: true,
+      },
+    },
+    {
+      plugins: {
+        "better-tailwindcss": eslintPluginBetterTailwindcss,
+      },
+      settings: {
+        "better-tailwindcss": {
+          entryPoint: "app/assets/css/main.css",
+        },
+      },
+      rules: {
+        "node/prefer-global/process": "off",
+        "better-tailwindcss/enforce-consistent-class-order": "warn",
+        "better-tailwindcss/no-duplicate-classes": "warn",
+        "better-tailwindcss/no-unnecessary-whitespace": "warn",
       },
     },
   ),
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/ui",
+  ],
 
   devtools: {
     enabled: true,
   },
+
+  css: ["~/assets/css/main.css"],
+
   compatibilityDate: "2025-07-15",
 
   eslint: {
